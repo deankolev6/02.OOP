0688f6fb402707b966ff0a5eada8513bce1c366b
deankolev6 <114641015+deankolev6@users.noreply.github.com>
2022-09-29 08:24:33 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex01
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        public Person()
+        {
+        }
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public void IntroduceYourself()
+        {
+            Console.WriteLine("Здравейте! Аз съм {0} и съм на {1} години.", name, age);
+        }
+    }
+}
